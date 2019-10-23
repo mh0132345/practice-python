30bc3ee181631c1175f0ff245ce40065c6050915
e1601133 <e1601133@edu.vamk.fi>
2019-10-23 23:56:52 +0300

    Remove codefights

--- codefights/hash_tables/areFollowingPatterns.py
+++
@@ -1,7 +0,0 @@
-def areFollowingPatterns(strings, patterns):
-    group = {}
-    for string, pattern in zip(strings, patterns):
-        if string in group and group[string] != pattern:
-            return False
-        group[string] = pattern
-    return len(group) == len(set(group.values()))

c768278bc2031054ba843058b611b109b5a8b44d
Hoang Le Minh <leminhhoang2509@gmail.com>
2020-05-14 18:12:25 +0700

    Update script1.py

--- other/database/PostgreSQL/script1.py
+++ other/database/PostgreSQL/script1.py
@@ -1,21 +1,21 @@
 import psycopg2
 
 def create_table():
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='27tk6ltk' host='localhost' port='5432'")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item, quantity, price):
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='27tk6ltk' host='localhost' port='5432'")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute("INSERT INTO store VALUES (%s,%s,%s)", (item, quantity, price))
     conn.commit()
     conn.close()
 
 def view():
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='27tk6ltk' host='localhost' port='5432'")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='' host='localhost' port='5432'")
     curr=conn.cursor()
     curr.execute("SELECT * FROM store")
     rows=curr.fetchall()
@@ -23,14 +23,14 @@ def view():
     return rows
 
 def delete(item):
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='27tk6ltk' host='localhost' port='5432'")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='' host='localhost' port='5432'")
     curr=conn.cursor()
     curr.execute("DELETE FROM store WHERE item=%s",(item,))
     conn.commit()
     conn.close()
 
 def update(quantity, price, item):
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='27tk6ltk' host='localhost' port='5432'")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='' host='localhost' port='5432'")
     curr=conn.cursor()
     curr.execute("UPDATE store SET quantity=%s, price=%s WHERE item=%s",(quantity, price, item))
     conn.commit()
